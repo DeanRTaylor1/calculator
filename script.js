1b706f82ab7110795c12c28689d4a05238457f43
Dean Taylor <deanrtaylor@hotmail.com>
2022-07-13 20:23:49 +0700

    number input added and error message if over 9 characters

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ const grid = document.querySelector("#grid");
 const output = document.querySelector("#calc-output");
 const currentCalc = document.querySelector("#calc-current");
 const resetbuttons = document.querySelector("#calc-top-row");
+const sevenbutton = document.getElementById("7");
+const numberButtons = document.querySelector("#numberbtn");
+let button;
+let result;
 
 
 
+
+
+// respond to keyboard input
+document.addEventListener("keypress", function(event) {
+    console.log(event.key);
+    let newKey = event.key;
+    if (newKey === 'Enter'){
+        button = document.getElementById(`=`);
+        button.click();
+    }
+    else if(newKey === 'c'){
+        button = document.getElementById(`clearBtn`);
+        button.click();
+    }else if(document.getElementById(`${event.key}` === null)) {
+        return;
+    } else {
+    console.log(document.getElementById(`${event.key}`));
+    button = document.getElementById(`${event.key}`);
+    button.click();
+}
+    //console.log(document.getElementById(`${event.key}`);
+    
+  });
 
 
 
@@ -28,6 +55,7 @@ function operation(operation, num2, num1){
 
 resetbuttons.addEventListener('click', event => {  
     if(!event.target.closest('button')) return; 
+    
 
     const key = event.target;
     const displayValue = output.textContent;
@@ -53,25 +81,31 @@ resetbuttons.addEventListener('click', event => {
 
 grid.addEventListener('click', event => {  
     if(!event.target.closest('button')) return;  
+    
 
     const key = event.target;
     const keyValue = key.textContent;
     const displayValue = output.textContent;
     const calculation = currentCalc.textContent;
-    console.log(key.classList.value);
+    //console.log(key.classList.value);
+    
 
     if(keyValue === '.' && grid.dataset.dot != '.' && grid.dataset.previousoperatorType != '='){
+        
         grid.dataset.dot = keyValue;
         output.textContent = displayValue + keyValue;
     } else if(keyValue === '.' && grid.dataset.dot === '.'){
         return
     }
 
-
+    console.log(displayValue.length); 
     if(key.classList.value === 'numberbtn' && keyValue != '.'){
+        
         if(displayValue === '0'){
             output.textContent = keyValue;  
         } else if (grid.dataset.previousoperatorType === '='){
+            return;
+        } else if (displayValue.length >= '9'){
             return;
         }else {output.textContent = displayValue + keyValue;
         }
@@ -107,13 +141,16 @@ grid.addEventListener('click', event => {
             return;
         }
         else { 
-            let result = operation(grid.dataset.previousoperatorType, parseFloat(displayValue), parseFloat(calculation));
+            result = operation(grid.dataset.previousoperatorType, parseFloat(displayValue), parseFloat(calculation));
             currentCalc.textContent = `${calculation} ${displayValue} ${keyValue}`
             output.textContent = result;
             grid.dataset.previousoperatorType = keyValue;
+            if(output.textContent.length > 9){
+                output.textContent ='Error';
+            }
 
         }
-
+        
     }
  
 });
